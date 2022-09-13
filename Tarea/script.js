c6a4a9004aea099e91b57796d920da774925a4aa
angeldarlincp <113081385+angeldarlincp@users.noreply.github.com>
2022-09-13 00:12:04 -0400

    Add files via upload

---
+++ Tarea/script.js
@@ -0,0 +1,26 @@
+function sumar() {
+  var x = parseInt(document.getElementById('vol1').value);
+  var y = parseInt(document.getElementById('vol2').value);
+  alert(x+ y);
+}
+
+function restar() {
+  var x = parseInt(document.getElementById('vol1').value);
+  var y = parseInt(document.getElementById('vol2').value);
+ alert(x- y);
+  
+}
+
+function multiplicar() {
+  var x = parseInt(document.getElementById('vol1').value);
+  var y = parseInt(document.getElementById('vol2').value);
+alert(x* y);
+  
+}
+
+function dividir() {
+  var x = parseInt(document.getElementById('vol1').value);
+  var y = parseInt(document.getElementById('vol2').value);
+alert(x/ y);
+  
+}
